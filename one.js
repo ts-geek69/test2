6fc82ecaef9a7d4ae58986bdde1c4051d709ffb4
ts-geek69 <madhujeet.valsa@techstaunch.com>
2024-10-18 12:42:18 +0530

    divide logic is done and log also

--- one.js
+++ one.js
@@ -3,5 +3,6 @@ console.log("Hello world! 2");
 console.log("Hello world! jeetu");
 console.log("substract complete!");
 console.log("multi complete!");
+console.log("divide complete!");
 
 
--- two.js
+++ two.js
@@ -7,3 +7,9 @@ const subtract = (a,b)=>{
 const multiply = (a,b)=>{
     return a*b;
 }
+const divide = (a,b)=>{
+    if(b===0){
+        throw new Error("Cannot divide by zero");
+    }
+    return a/b;
+}
